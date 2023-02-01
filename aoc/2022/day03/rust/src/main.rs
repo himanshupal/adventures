9c5c8332e5ba83d2144928313ee9c30a8cd0d341
Himanshu Pal <mailtohimanshupal@gmail.com>
2023-02-01 09:24:43 +0530

    Added rust solution to day03 part two

--- aoc/2022/day03/rust/src/main.rs
+++ aoc/2022/day03/rust/src/main.rs
@@ -1,27 +1,48 @@
 use std::fs;
 
 fn main() {
-    if let Ok(data) = fs::read_to_string("../input.txt") {
-        let sum = data
-            .lines()
-            .map(|text| {
-                let text_length = text.len();
-                let half_length = text_length / 2;
-                let first_part = &text[0..half_length];
-                let second_part = &text[half_length..text_length];
+    let Ok(data) = fs::read_to_string("../input.txt") else { return };
 
-                for char in first_part.chars() {
-                    if second_part.contains(char) {
-                        return get_weight(char);
-                    }
+    let sum = data
+        .lines()
+        .map(|text| {
+            let text_length = text.len();
+            let half_length = text_length / 2;
+            let first_part = &text[0..half_length];
+            let second_part = &text[half_length..text_length];
+
+            for char in first_part.chars() {
+                if second_part.contains(char) {
+                    return get_weight(char);
                 }
+            }
 
-                0
-            })
-            .sum::<u32>();
+            0
+        })
+        .sum::<u32>();
 
-        println!("Sum: {sum}")
+    let lines = data.lines().collect::<Vec<&str>>();
+    let mut it = lines.chunks(3);
+    let mut sum_part2 = 0;
+
+    loop {
+        let Some(broken_lines) = it.next() else {break};
+
+        let first_line = broken_lines.get(0).unwrap();
+        let second_line = broken_lines.get(1).unwrap();
+        let third_line = broken_lines.get(2).unwrap();
+
+        for char in first_line.chars() {
+            if second_line.contains(char) {
+                if third_line.contains(char) {
+                    sum_part2 += get_weight(char);
+                    break; // Only need to be read once
+                }
+            }
+        }
     }
+
+    println!("Sum: {sum} Part2: {sum_part2}");
 }
 
 fn get_weight(ch: char) -> u32 {
