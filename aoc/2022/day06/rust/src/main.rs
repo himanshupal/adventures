324cb6b5648dac63467328e052d06cc6fd78294d
Himanshu Pal <mailtohimanshupal@gmail.com>
2023-03-12 15:39:23 +0530

    Added rust solution to day06 problem2

--- aoc/2022/day06/rust/src/main.rs
+++ aoc/2022/day06/rust/src/main.rs
@@ -2,12 +2,16 @@ use std::{fs, ops::Shl};
 
 fn main() {
     let Ok(data) = fs::read_to_string("../input.txt") else {return};
-    let mut pos = 0;
 
-    for i in 0..(data.len() - 3) {
+    println!("Problem1: {}", get_marker(&data, 4));
+    println!("Problem2: {}", get_marker(&data, 14));
+}
+
+fn get_marker(data: &str, pos_init: usize) -> usize {
+    for i in 0..(data.len() - (pos_init - 1)) {
         let mut map: i32 = 0;
         let mut repeated = false;
-        let slice = &data[i..i + 4];
+        let slice = &data[i..(i + pos_init)];
         for ch in slice.chars() {
             let mask = 1.shl(ch as i32 - b'a' as i32);
             if map & mask != 0 {
@@ -17,10 +21,8 @@ fn main() {
             map |= mask;
         }
         if !repeated {
-            pos = i + 4;
-            break;
+            return i + pos_init;
         }
     }
-
-    println!("Problem1: {pos}");
+    0
 }
