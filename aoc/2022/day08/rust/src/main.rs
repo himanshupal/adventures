290921310e2e8e21fcf97d18e2b48bafb2974d62
Himanshu Pal <mailtohimanshupal@gmail.com>
2023-04-06 00:38:35 +0530

    Added rust solution to day08 problem2

--- aoc/2022/day08/rust/src/main.rs
+++ aoc/2022/day08/rust/src/main.rs
@@ -13,6 +13,8 @@ fn main() {
 
     let mut visibility = vec![0u128; no_of_lines];
 
+    let mut max_score = 0;
+
     for (line_number, line) in data.lines().enumerate() {
         if line_number == 0 || line_number == last_line_index {
             visibility[line_number] |= (1 << text_length) - 1;
@@ -28,6 +30,7 @@ fn main() {
             }
 
             let mut visible = 0;
+            let (mut t, mut r, mut b, mut l) = (0, 0, 0, 0);
 
             for distance in 0..no_of_lines {
                 let next_location = distance + 1;
@@ -35,35 +38,39 @@ fn main() {
                 // println!(">>> {line_number}:{index}:{distance}:{underlying}");
 
                 if (visible & (1 << 3)) == 0 && line_number >= next_location && next_location < last_line_index {
-                    let value = lines[distance].chars().nth(index).expect("unreachable index").to_digit(10).expect("NaN");
+                    let value = get_char_at_index(lines[line_number - next_location], index);
                     // println!("T {value}");
                     if value >= underlying {
                         visible |= 1 << 3;
                     }
+                    t += 1;
                 }
 
                 if (visible & (1 << 2)) == 0 && next_location > index && next_location <= last_char_index {
-                    let value = lines[line_number].chars().nth(next_location).expect("unreachable index").to_digit(10).expect("NaN");
+                    let value = get_char_at_index(lines[line_number], next_location);
                     // println!("R {value}");
                     if value >= underlying {
                         visible |= 1 << 2;
                     }
+                    r += 1;
                 }
 
                 if (visible & (1 << 1)) == 0 && line_number < distance {
-                    let value = lines[distance].chars().nth(index).expect("unreachable index").to_digit(10).expect("NaN");
+                    let value = get_char_at_index(lines[distance], index);
                     // println!("B {value}");
                     if value >= underlying {
                         visible |= 1 << 1;
                     }
+                    b += 1;
                 }
 
                 if (visible & (1 << 0)) == 0 && index >= next_location {
-                    let value = lines[line_number].chars().nth(index - next_location).expect("unreachable index").to_digit(10).expect("NaN");
+                    let value = get_char_at_index(lines[line_number], index - next_location);
                     // println!("L {value}");
                     if value >= underlying {
                         visible |= 1 << 0;
                     }
+                    l += 1;
                 }
             }
 
@@ -71,7 +78,11 @@ fn main() {
                 visibility[line_number] |= 1 << (last_char_index - index);
             }
 
-            // println!("{:b}\n", visible);
+            let score = t * r * b * l;
+
+            if score > max_score {
+                max_score = score;
+            }
         }
     }
 
@@ -79,7 +90,11 @@ fn main() {
     //     println!("{:b}", row);
     // }
 
-    println!("part_01: {:?}", visibility.iter().map(|m| count_bits(*m)).sum::<u128>());
+    println!("part_01: {:?}, part_02: {max_score}", visibility.iter().map(|m| count_bits(*m)).sum::<u128>());
+}
+
+fn get_char_at_index(text: &str, index: usize) -> u32 {
+    text.chars().nth(index).expect("unreachable index").to_digit(10).expect("NaN")
 }
 
 fn count_bits(mut value: u128) -> u128 {
