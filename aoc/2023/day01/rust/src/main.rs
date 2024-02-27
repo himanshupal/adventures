68742ddff4f3d8653d14bc5e63af3eca37a3e6e8
Himanshu Pal <mailtohimanshupal@gmail.com>
2024-02-27 21:28:52 +0530

    Added 2023:day01 part 2 rust solution

--- aoc/2023/day01/rust/src/main.rs
+++ aoc/2023/day01/rust/src/main.rs
@@ -1,14 +1,15 @@
-use std::fs;
+use std::{cmp::Ordering, fs};
 
 fn main() {
     let Ok(data) = fs::read_to_string("../input.txt") else {
         panic!("Couldn't read input file");
     };
 
-    println!("Day 01: {}", day01(data));
+    println!("Part 1: {}", part1(&data));
+    println!("Part 2: {}", part2(&data));
 }
 
-fn day01(input: String) -> usize {
+fn part1(input: &String) -> usize {
     input
         .lines()
         .enumerate()
@@ -28,6 +29,81 @@ fn day01(input: String) -> usize {
                 .chars()
                 .nth(last_digit_pos)
                 .expect(&format!("Failed to get last char of line {n}"));
+
+            format!("{first_digit}{last_digit}")
+                .parse::<usize>()
+                .expect("Failed to parse back to digit at index {n}")
+        })
+        .sum()
+}
+
+fn part2(input: &String) -> usize {
+    let numbers = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    input
+        .lines()
+        .map(|line| {
+            let chars: Vec<char> = line.chars().collect();
+
+            let mut values = (3..=5)
+                .map(|size| {
+                    chars
+                        .windows(size)
+                        .enumerate()
+                        .filter_map(|(n, chunk)| {
+                            let chunk_as_str = String::from_iter(chunk);
+
+                            if numbers.contains(&chunk_as_str.as_str()) {
+                                Some((n, chunk_as_str))
+                            } else {
+                                None
+                            }
+                        })
+                        .collect::<Vec<(usize, String)>>()
+                })
+                .fold(vec![], |mut p, mut c| {
+                    p.append(&mut c);
+                    p
+                });
+
+            if let Some(value) = line.find(|x| char::is_digit(x, 10)) {
+                values.push((value, line.chars().nth(value).unwrap().to_string()))
+            }
+
+            if let Some(value) = line.rfind(|x| char::is_digit(x, 10)) {
+                values.push((value, line.chars().nth(value).unwrap().to_string()))
+            }
+
+            values.sort_by(|a, b| {
+                if a.0 < b.0 {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
+            });
+
+            values
+        })
+        .map(|v| {
+            let first_digit = if let Some(value) = numbers
+                .iter()
+                .position(|c| c == &v.first().expect("No first value").1)
+            {
+                value + 1
+            } else {
+                v.first().unwrap().1.parse().unwrap()
+            };
+
+            let last_digit = if let Some(value) = numbers
+                .iter()
+                .position(|c| c == &v.last().expect("No last value").1)
+            {
+                value + 1
+            } else {
+                v.last().unwrap().1.parse().unwrap()
+            };
 
             format!("{first_digit}{last_digit}")
                 .parse::<usize>()
